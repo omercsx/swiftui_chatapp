bbdb9cfefa67b9d8b75c348eab49cac10f445d8a
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-29 10:20:58 +0300

    Initial Commit

---
+++ swiftui_chatapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui_chatapp
+//
+//  Created by Omer Cagri Sayir on 29.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui_chatapp/swiftui_chatappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_chatappApp.swift
+//  swiftui_chatapp
+//
+//  Created by Omer Cagri Sayir on 29.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_chatappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
