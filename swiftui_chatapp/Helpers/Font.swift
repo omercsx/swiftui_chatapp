a69c403f1bed4bade8992e58e25ada091af1e354
Omer Cagri Sayir <omercagris@gmail.com>
2024-02-03 12:58:36 +0300

    font names changed

--- swiftui_chatapp/Helpers/Font.swift
+++ swiftui_chatapp/Helpers/Font.swift
@@ -9,34 +9,34 @@ import Foundation
 import SwiftUI
 
 extension Font {
-    public static var body: Font {
+    public static var bodyParagraph: Font {
         return Font.custom("LexendDeca-Regular", size: 14)
     }
-    
+
     public static var button: Font {
         return Font.custom("LexendDeca-SemiBold", size: 14)
     }
-    
-    public static var caption: Font {
+
+    public static var smallText: Font {
         return Font.custom("LexendDeca-Regular", size: 10)
     }
-    
+
     public static var tabBar: Font {
         return Font.custom("LexendDeca-Regular", size: 12)
     }
-    
+
     public static var settings: Font {
         return Font.custom("LexendDeca-Regular", size: 16)
     }
-    
-    public static var title: Font {
+
+    public static var titleText: Font {
         return Font.custom("LexendDeca-Bold", size: 23)
     }
-    
+
     public static var pageTitle: Font {
         return Font.custom("LexendDeca-SemiBold", size: 33)
     }
-    
+
     public static var chatHeading: Font {
         return Font.custom("LexendDeca-SemiBold", size: 19)
     }
